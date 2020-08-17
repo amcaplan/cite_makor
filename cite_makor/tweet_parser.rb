5c7c2ada129050581a968b771886a96d7cdbe9c1
Ariel Caplan <ariel.caplan@cloudinary.com>
2020-08-17 18:14:19 +0300

    new strategy to force explicit citing and avoid false positives

--- cite_makor/tweet_parser.rb
+++ cite_makor/tweet_parser.rb
@@ -21,31 +21,22 @@ module CiteMakor
     def ref_text
       @ref_text ||=
         begin
-          if relevant_text.any? { |word| word.start_with?('@') }
-            CiteMakor::CustomLogger.info "Mentioned other users after me, this is probably a reply, not answering. Tweet text:\n#{tweet_text}"
-            []
-          else
-            text = relevant_text.reject { |i| i.match?(/\A(please|cite|for|me|https:\/\/t\.co\/\w+)\z/i) }
-            text.reject! do |item|
-              if matchdata = item.match(/\Alang(uage)?=(?<lang>he|en)/)
-                @lang = matchdata[:lang]
-              end
+          text = relevant_text.reject { |i| i.match?(/\A(please|cite|for|me|https:\/\/t\.co\/\w+|@\w+)\z/i) }
+          text.reject! do |item|
+            if matchdata = item.match(/\Alang(uage)?=(?<lang>he|en)/)
+              @lang = matchdata[:lang]
             end
-            text
           end
+          text
         end
     end
 
     def relevant_text
       @relevant_text ||=
         begin
-          last_mention_index = split_text.rindex { |word| word.match?(/\A@citemakor\z/i) } || -1
-          split_text[(last_mention_index + 1)..-1]
+          match = tweet_text.strip.gsub(/\s+/, ' ').match(/@CiteMakor(\splease)?\scite(\sfor)?(\sme)?\s(?<ref_text>[^\n]+)/)
+          (match&.[](:ref_text) || "").split(' ')
         end
-    end
-
-    def split_text
-      @split_text ||= tweet_text.strip.split(/\s+/)
     end
   end
 end
--- spec/tweet_parser_spec.rb
+++ spec/tweet_parser_spec.rb
@@ -9,9 +9,12 @@ describe CiteMakor::TweetParser do
 
   [
     ["@CiteMakor please cite for me Genesis 1:1", "Genesis 1:1"],
+    ["@CiteMakor please cite Genesis 1:1", "Genesis 1:1", details: "can leave out for me"],
+    ["@CiteMakor Genesis 1:1", "", details: "need to say cite"],
+    ["pleace cite @CiteMakor Genesis 1:1", "", details: "ignores cite before name"],
     ["@CiteMakor @amcaplan what are it's limits?", "", details: "ignoring a reply to someone else"],
     ["@CiteMakor @amcaplan @SefariaProject @CiteMakor cite for me בבא קמא ד ע\"א", "בבא קמא ד ע\"א", "replying to a reply to me"],
-    ["I was gonna cosplay sailor moon, but...\n\n@CiteMakor Deuteronomy 22:5", "Deuteronomy 22:5", "excluding preface"],
+    ["I was gonna cosplay sailor moon, but...\n\n@CiteMakor cite me Deuteronomy 22:5", "Deuteronomy 22:5", "excluding preface"],
     ["@CiteMakor please cite for me Mishnah Sotah 3: 4 https://t.co/uk8YssrQDj", "Mishnah Sotah 3: 4", "excluding a twitter image/quote tweet/link"],
 
     # language tests
